e34ed7cbcc1649d6b0d2e6de4c02f014fd5da3e7
justfortestingnothibghere <armanhacker900@gmail.com>
2026-05-06 18:26:32 +0530

    Update __init__.py

--- TeamDevXMusic/modules/plugins/__init__.py
+++ TeamDevXMusic/modules/plugins/__init__.py
@@ -1,1 +1,55 @@
+"""
+                      [TeamDev](https://t.me/team_x_og)
+          
+          Project Id -> 39.
+          Project Name -> TeamDev X Music
+          Project Age -> 15D+ (Updated On 05/05/2026)
+          Project Idea By -> @MR_ARMAN_08
+          Project Dev -> @MR_ARMAN_08
+          Powered By -> @Team_X_Og ( On Telegram )
+          Updates -> @TeamDevXBots ( On telegram )
+    
+    Setup Guides -> Read > README.md
+    
+          This Script Part Off https://t.me/Team_X_Og's Team.
+          Copyright ©️ 2026 TeamDev | @Team_X_Og
+          
+    • Some Quick Help
+    - Read Full README.md For Understanding The Content.
+    - If You Need Any Help Contact Us In @Team_X_Og's Group
+    
+         Compatible In BotApi 9.5 Fully
+         Build For BotApi 9.5
+         We'll Keep Update This Repo If We Got 30+ Stars In One Month Of Release.
+"""
+
+# Copyright (C) 2025 by @MR_ARMAN_08 @ Github, < https://github.com/justfortestingnothibghere >
+# All rights reserved. © TeamDevXMusic
+
+"""
+TeamDevXMusic is a Telegram Music Bot by @MR_ARMAN_08.
+Copyright (c) 2026 ~ Present Team TeamDevXMusic <https://github.com/justfortestingnothibghere>
+
+This program is "Ristiricted" software: Don't Modify, Redistribute Or Sell This Project Proving As Your Project
+TeamDev Will Take Action If They Find Any unauthorised usages.
+"""
+
+
+import glob
+from os.path import dirname, isfile
+
+
+def __list_all_modules():
+    work_dir = dirname(__file__)
+    mod_paths = glob.glob(f"{work_dir}/*/*.py")
+
+    return [
+        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
+        for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+
+
+ALL_MODULES = sorted(__list_all_modules())
+__all__ = ALL_MODULES + ["ALL_MODULES"]
 
