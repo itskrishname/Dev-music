bd88b83faa1b4e39f5b46f393adf32f67d334a80
justfortestingnothibghere <armanhacker900@gmail.com>
2026-05-06 18:37:37 +0530

    Add files via upload

---
+++ TeamDevXMusic/toolkit/engine/streamer.py
@@ -0,0 +1,458 @@
+"""
+                      [TeamDev](https://t.me/team_x_og)
+          
+          Project Id -> 39.
+          Project Name -> TeamDev X Music
+          Project Age -> 15D+ (Updated On 05/05/2026)
+          Project Idea By -> @MR_ARMAN_08
+          Project Dev -> @MR_ARMAN_08
+          Powered By -> @Team_X_Og ( On Telegram )
+          Updates -> @TeamDevXBots ( On telegram )
+    
+    Setup Guides -> Read > README.md
+    
+          This Script Part Off https://t.me/Team_X_Og's Team.
+          Copyright ©️ 2026 TeamDev | @Team_X_Og
+          
+    • Some Quick Help
+    - Read Full README.md For Understanding The Content.
+    - If You Need Any Help Contact Us In @Team_X_Og's Group
+    
+         Compatible In BotApi 9.5 Fully
+         Build For BotApi 9.5
+         We'll Keep Update This Repo If We Got 30+ Stars In One Month Of Release.
+"""
+
+# Copyright (C) 2025 by @MR_ARMAN_08 @ Github, < https://github.com/justfortestingnothibghere >
+# All rights reserved. © TeamDevXMusic
+
+"""
+TeamDevXMusic is a Telegram Music Bot by @MR_ARMAN_08.
+Copyright (c) 2026 ~ Present Team TeamDevXMusic <https://github.com/justfortestingnothibghere>
+
+This program is "Ristiricted" software: Don't Modify, Redistribute Or Sell This Project Proving As Your Project
+TeamDev Will Take Action If They Find Any unauthorised usages.
+"""
+
+
+import os
+from random import randint
+from typing import Union
+
+from pyrogram.types import InlineKeyboardMarkup
+
+import config
+from TeamDevXMusic import Carbon, YouTube, app
+from TeamDevXMusic.relay.caller import TeamDevXMusic
+from TeamDevXMusic.helpers import db
+from TeamDevXMusic.toolkit.datastore import (
+    add_active_chat,
+    add_active_video_chat,
+    is_active_chat,
+    is_video_allowed,
+    music_on,
+)
+from TeamDevXMusic.toolkit.errors import AssistantErr
+from TeamDevXMusic.toolkit.panels.play import stream_markup, queue_markup, telegram_markup
+from TeamDevXMusic.toolkit.panels.playlist import close_markup
+from TeamDevXMusic.toolkit.paste import TeamDevXMusicbin
+from TeamDevXMusic.toolkit.engine.trackqueue import put_queue, put_queue_index
+from TeamDevXMusic.toolkit.thumbgen import gen_thumb, gen_qthumb
+
+
+async def stream(
+    _,
+    mystic,
+    user_id,
+    result,
+    chat_id,
+    user_name,
+    original_chat_id,
+    video: Union[bool, str] = None,
+    streamtype: Union[bool, str] = None,
+    spotify: Union[bool, str] = None,
+    forceplay: Union[bool, str] = None,
+):
+    if not result:
+        return
+    if video and not await is_video_allowed(chat_id):
+        raise AssistantErr(_["play_7"])
+    if forceplay:
+        await TeamDevXMusic.force_stop_stream(chat_id)
+    if streamtype == "playlist":
+        msg = f"{_['playlist_16']}\n\n"
+        count = 0
+        for search in result:
+            if int(count) == config.PLAYLIST_FETCH_LIMIT:
+                continue
+            try:
+                (
+                    title,
+                    duration_min,
+                    duration_sec,
+                    thumbnail,
+                    vidid,
+                ) = await YouTube.details(search, not spotify)
+            except Exception:
+                continue
+            if str(duration_min) == "None":
+                continue
+            if duration_sec > config.DURATION_LIMIT:
+                continue
+            if await is_active_chat(chat_id):
+                await put_queue(
+                    chat_id,
+                    original_chat_id,
+                    f"vid_{vidid}",
+                    title,
+                    duration_min,
+                    user_name,
+                    vidid,
+                    user_id,
+                    "video" if video else "audio",
+                )
+                position = len(db.get(chat_id)) - 1
+                count += 1
+                msg += f"{count}- {title[:70]}\n"
+                msg += f"{_['playlist_17']} {position}\n\n"
+            else:
+                if not forceplay:
+                    db[chat_id] = []
+                status = True if video else None
+                try:
+                    file_path, direct = await YouTube.download(
+                        vidid, mystic, video=status, videoid=True
+                    )
+                except:
+                    raise AssistantErr(_["play_16"])
+                await TeamDevXMusic.join_call(
+                    chat_id, original_chat_id, file_path, video=status, image=thumbnail
+                )
+                await put_queue(
+                    chat_id,
+                    original_chat_id,
+                    file_path if direct else f"vid_{vidid}",
+                    title,
+                    duration_min,
+                    user_name,
+                    vidid,
+                    user_id,
+                    "video" if video else "audio",
+                    forceplay=forceplay,
+                )
+                # theme = await check_theme(chat_id)
+                img = await gen_thumb(vidid, user_name)
+                button = stream_markup(_, vidid, chat_id)
+                run = await app.send_photo(
+                    original_chat_id,
+                    photo=img,
+                    caption=_["stream_1"].format(
+                        title[:27],
+                        f"https://t.me/{app.username}?start=info_{vidid}",
+                        duration_min,
+                        user_name,
+                    ),
+                    reply_markup=InlineKeyboardMarkup(button),
+                )
+                db[chat_id][0]["mystic"] = run
+                db[chat_id][0]["markup"] = "stream"
+        if count == 0:
+            return
+        link = await TeamDevXMusicbin(msg)
+        lines = msg.count("\n")
+        car = os.linesep.join(msg.split(os.linesep)[:17]) if lines >= 17 else msg
+        carbon = await Carbon.generate(car, randint(100, 10000000))
+        upl = close_markup(_)
+        return await app.send_photo(
+            original_chat_id,
+            photo=carbon,
+            caption=_["playlist_18"].format(position, link),
+            reply_markup=upl,
+        )
+    elif streamtype == "youtube":
+        link = result["link"]
+        vidid = result["vidid"]
+        title = (result["title"]).title()
+        duration_min = result["duration_min"]
+        thumbnail = result["thumb"]
+        status = True if video else None
+        # Track for AutoPlay
+        from TeamDevXMusic.toolkit.datastore import set_autoplay_last
+        set_autoplay_last(chat_id, vidid)
+        try:
+            file_path, direct = await YouTube.download(
+                vidid, mystic, videoid=True, video=status
+            )
+        except:
+            raise AssistantErr(_["play_16"])
+        if await is_active_chat(chat_id):
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                file_path if direct else f"vid_{vidid}",
+                title,
+                duration_min,
+                user_name,
+                vidid,
+                user_id,
+                "video" if video else "audio",
+            )
+            # theme = await check_theme(chat_id)
+            position = len(db.get(chat_id)) - 1
+            qimg = await gen_qthumb(vidid, user_name)
+            button = queue_markup(_, vidid, chat_id)
+            run = await app.send_photo(
+                original_chat_id,
+                photo=qimg,
+                caption=_["queue_4"].format(
+                    position, title[:27], duration_min, user_name
+                ),
+                reply_markup=InlineKeyboardMarkup(button),
+            )
+        else:
+            if not forceplay:
+                db[chat_id] = []
+            await TeamDevXMusic.join_call(
+                chat_id, original_chat_id, file_path, video=status, image=thumbnail
+            )
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                file_path if direct else f"vid_{vidid}",
+                title,
+                duration_min,
+                user_name,
+                vidid,
+                user_id,
+                "video" if video else "audio",
+                forceplay=forceplay,
+            )
+            # theme = await check_theme(chat_id)
+            img = await gen_thumb(vidid, user_name)
+            button = stream_markup(_, vidid, chat_id)
+            try:
+                run = await app.send_photo(
+                    original_chat_id,
+                    photo=img,
+                    caption=_["stream_1"].format(
+                        title[:27],
+                        f"https://t.me/{app.username}?start=info_{vidid}",
+                        duration_min,
+                        user_name,
+                    ),
+                    reply_markup=InlineKeyboardMarkup(button),
+                )
+                db[chat_id][0]["mystic"] = run
+                db[chat_id][0]["markup"] = "stream"
+            except Exception as ex:
+                print(ex)
+    elif streamtype == "soundcloud":
+        file_path = result["filepath"]
+        title = result["title"]
+        duration_min = result["duration_min"]
+        if await is_active_chat(chat_id):
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                file_path,
+                title,
+                duration_min,
+                user_name,
+                streamtype,
+                user_id,
+                "audio",
+            )
+            position = len(db.get(chat_id)) - 1
+            await app.send_message(
+                original_chat_id,
+                _["queue_4"].format(position, title[:30], duration_min, user_name),
+            )
+        else:
+            if not forceplay:
+                db[chat_id] = []
+            await TeamDevXMusic.join_call(chat_id, original_chat_id, file_path, video=None)
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                file_path,
+                title,
+                duration_min,
+                user_name,
+                streamtype,
+                user_id,
+                "audio",
+                forceplay=forceplay,
+            )
+            button = telegram_markup(_, chat_id)
+            run = await app.send_photo(
+                original_chat_id,
+                photo=config.SOUNCLOUD_IMG_URL,
+                caption=_["stream_3"].format(title, duration_min, user_name),
+                reply_markup=InlineKeyboardMarkup(button),
+            )
+            db[chat_id][0]["mystic"] = run
+            db[chat_id][0]["markup"] = "tg"
+    elif streamtype == "telegram":
+        file_path = result["path"]
+        link = result["link"]
+        title = (result["title"]).title()
+        duration_min = result["dur"]
+        status = True if video else None
+        if await is_active_chat(chat_id):
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                file_path,
+                title,
+                duration_min,
+                user_name,
+                streamtype,
+                user_id,
+                "video" if video else "audio",
+            )
+            position = len(db.get(chat_id)) - 1
+            await app.send_message(
+                original_chat_id,
+                _["queue_4"].format(position, title[:30], duration_min, user_name),
+            )
+        else:
+            if not forceplay:
+                db[chat_id] = []
+            await TeamDevXMusic.join_call(chat_id, original_chat_id, file_path, video=status)
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                file_path,
+                title,
+                duration_min,
+                user_name,
+                streamtype,
+                user_id,
+                "video" if video else "audio",
+                forceplay=forceplay,
+            )
+            if video:
+                await add_active_video_chat(chat_id)
+            button = telegram_markup(_, chat_id)
+            run = await app.send_photo(
+                original_chat_id,
+                photo=config.TELEGRAM_VIDEO_URL if video else config.TELEGRAM_AUDIO_URL,
+                caption=_["stream_4"].format(title, link, duration_min, user_name),
+                reply_markup=InlineKeyboardMarkup(button),
+            )
+            db[chat_id][0]["mystic"] = run
+            db[chat_id][0]["markup"] = "tg"
+    elif streamtype == "live":
+        link = result["link"]
+        vidid = result["vidid"]
+        title = (result["title"]).title()
+        thumbnail = result["thumb"]
+        duration_min = "00:00"
+        status = True if video else None
+        if await is_active_chat(chat_id):
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                f"live_{vidid}",
+                title,
+                duration_min,
+                user_name,
+                vidid,
+                user_id,
+                "video" if video else "audio",
+            )
+            position = len(db.get(chat_id)) - 1
+            await app.send_message(
+                original_chat_id,
+                _["queue_4"].format(position, title[:30], duration_min, user_name),
+            )
+        else:
+            if not forceplay:
+                db[chat_id] = []
+            n, file_path = await YouTube.video(link)
+            if n == 0:
+                raise AssistantErr(_["str_3"])
+            await TeamDevXMusic.join_call(
+                chat_id,
+                original_chat_id,
+                file_path,
+                video=status,
+                image=thumbnail or None,
+            )
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                f"live_{vidid}",
+                title,
+                duration_min,
+                user_name,
+                vidid,
+                user_id,
+                "video" if video else "audio",
+                forceplay=forceplay,
+            )
+            # theme = await check_theme(chat_id)
+            img = await gen_thumb(vidid, user_name)
+            button = telegram_markup(_, chat_id)
+            run = await app.send_photo(
+                original_chat_id,
+                photo=img,
+                caption=_["stream_1"].format(
+                    title[:27],
+                    f"https://t.me/{app.username}?start=info_{vidid}",
+                    duration_min,
+                    user_name,
+                ),
+                reply_markup=InlineKeyboardMarkup(button),
+            )
+            db[chat_id][0]["mystic"] = run
+            db[chat_id][0]["markup"] = "tg"
+    elif streamtype == "index":
+        link = result
+        title = "Index or M3u8 Link"
+        duration_min = "URL stream"
+        if await is_active_chat(chat_id):
+            await put_queue_index(
+                chat_id,
+                original_chat_id,
+                "index_url",
+                title,
+                duration_min,
+                user_name,
+                link,
+                "video" if video else "audio",
+            )
+            position = len(db.get(chat_id)) - 1
+            await mystic.edit_text(
+                _["queue_4"].format(position, title[:30], duration_min, user_name)
+            )
+        else:
+            if not forceplay:
+                db[chat_id] = []
+            await TeamDevXMusic.join_call(
+                chat_id,
+                original_chat_id,
+                link,
+                video=True if video else None,
+            )
+            await put_queue_index(
+                chat_id,
+                original_chat_id,
+                "index_url",
+                title,
+                duration_min,
+                user_name,
+                link,
+                "video" if video else "audio",
+                forceplay=forceplay,
+            )
+            button = telegram_markup(_, chat_id)
+            run = await app.send_photo(
+                original_chat_id,
+                photo=config.STREAM_IMG_URL,
+                caption=_["stream_2"].format(user_name),
+                reply_markup=InlineKeyboardMarkup(button),
+            )
+            db[chat_id][0]["mystic"] = run
+            db[chat_id][0]["markup"] = "tg"
+            await mystic.delete()
---
+++ TeamDevXMusic/toolkit/engine/trackqueue.py
@@ -0,0 +1,121 @@
+"""
+                      [TeamDev](https://t.me/team_x_og)
+          
+          Project Id -> 39.
+          Project Name -> TeamDev X Music
+          Project Age -> 15D+ (Updated On 05/05/2026)
+          Project Idea By -> @MR_ARMAN_08
+          Project Dev -> @MR_ARMAN_08
+          Powered By -> @Team_X_Og ( On Telegram )
+          Updates -> @TeamDevXBots ( On telegram )
+    
+    Setup Guides -> Read > README.md
+    
+          This Script Part Off https://t.me/Team_X_Og's Team.
+          Copyright ©️ 2026 TeamDev | @Team_X_Og
+          
+    • Some Quick Help
+    - Read Full README.md For Understanding The Content.
+    - If You Need Any Help Contact Us In @Team_X_Og's Group
+    
+         Compatible In BotApi 9.5 Fully
+         Build For BotApi 9.5
+         We'll Keep Update This Repo If We Got 30+ Stars In One Month Of Release.
+"""
+
+# Copyright (C) 2025 by @MR_ARMAN_08 @ Github, < https://github.com/justfortestingnothibghere >
+# All rights reserved. © TeamDevXMusic
+
+"""
+TeamDevXMusic is a Telegram Music Bot by @MR_ARMAN_08.
+Copyright (c) 2026 ~ Present Team TeamDevXMusic <https://github.com/justfortestingnothibghere>
+
+This program is "Ristiricted" software: Don't Modify, Redistribute Or Sell This Project Proving As Your Project
+TeamDev Will Take Action If They Find Any unauthorised usages.
+"""
+
+from typing import Union
+
+from config import autoclean, chatstats, userstats
+from config.config import time_to_seconds
+from TeamDevXMusic.helpers import db
+
+
+async def put_queue(
+    chat_id,
+    original_chat_id,
+    file,
+    title,
+    duration,
+    user,
+    vidid,
+    user_id,
+    stream,
+    forceplay: Union[bool, str] = None,
+):
+    title = title.title()
+    try:
+        duration_in_seconds = time_to_seconds(duration) - 3
+    except Exception:
+        duration_in_seconds = 0
+    put = {
+        "title": title,
+        "dur": duration,
+        "streamtype": stream,
+        "by": user,
+        "chat_id": original_chat_id,
+        "file": file,
+        "vidid": vidid,
+        "seconds": duration_in_seconds,
+        "played": 0,
+    }
+    if forceplay:
+        if check := db.get(chat_id):
+            check.insert(0, put)
+        else:
+            db[chat_id] = []
+            db[chat_id].append(put)
+    else:
+        db[chat_id].append(put)
+    autoclean.append(file)
+    vidid = "telegram" if vidid == "soundcloud" else vidid
+    to_append = {"vidid": vidid, "title": title}
+    if chat_id not in chatstats:
+        chatstats[chat_id] = []
+    chatstats[chat_id].append(to_append)
+    if user_id not in userstats:
+        userstats[user_id] = []
+    userstats[user_id].append(to_append)
+    return
+
+
+async def put_queue_index(
+    chat_id,
+    original_chat_id,
+    file,
+    title,
+    duration,
+    user,
+    vidid,
+    stream,
+    forceplay: Union[bool, str] = None,
+):
+    put = {
+        "title": title,
+        "dur": duration,
+        "streamtype": stream,
+        "by": user,
+        "chat_id": original_chat_id,
+        "file": file,
+        "vidid": vidid,
+        "seconds": 0,
+        "played": 0,
+    }
+    if forceplay:
+        if check := db.get(chat_id):
+            check.insert(0, put)
+        else:
+            db[chat_id] = []
+            db[chat_id].append(put)
+    else:
+        db[chat_id].append(put)
