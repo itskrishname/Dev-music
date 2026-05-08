5fcd92ea9bbe8666ba5790122dc353f8921c4543
Krishna <kd259457@gmail.com>
2026-05-08 23:55:41 +0530

    Update play.py

--- TeamDevXMusic/toolkit/panels/play.py
+++ TeamDevXMusic/toolkit/panels/play.py
@@ -70,7 +70,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
             ),
-            InlineKeyboardButton(text="• 𝖮𝗐𝗇𝖾𝗋", url="https://t.me/MR_ARMAN_08"),
+            InlineKeyboardButton(text="• 𝖮𝗐𝗇𝖾𝗋", url="https://t.me/Lord_Vasudev_Krishna"),
         ],
         [
             InlineKeyboardButton(
@@ -97,7 +97,7 @@ def telegram_markup_timer(_, videoid, chat_id, played, dur):
                 text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
             ),
-            InlineKeyboardButton(text="𝖮𝗐𝗇𝖾𝗋", url="https://t.me/MR_ARMAN_08"),
+            InlineKeyboardButton(text="𝖮𝗐𝗇𝖾𝗋", url="https://t.me/Lord_Vasudev_Krishna"),
         ],
         [
             InlineKeyboardButton(
@@ -115,7 +115,7 @@ def stream_markup(_, videoid, chat_id):
                 text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
             ),
-            InlineKeyboardButton(text="• 𝖮𝗐𝗇𝖾𝗋", url="https://t.me/MR_ARMAN_08"),
+            InlineKeyboardButton(text="• 𝖮𝗐𝗇𝖾𝗋", url="https://t.me/Lord_Vasudev_Krishna"),
         ],
         [
             InlineKeyboardButton(
