320073aa0df4813fd07e902dc60b8b6d7e4d13bd
Krishna <kd259457@gmail.com>
2026-05-09 00:02:45 +0530

    Update inlinesearch.py

--- TeamDevXMusic/toolkit/inlinesearch.py
+++ TeamDevXMusic/toolkit/inlinesearch.py
@@ -41,37 +41,37 @@ answer = [
     InlineQueryResultArticle(
         title="• ᴩᴀᴜsᴇ",
         description="ᴩᴀᴜsᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-        thumb_url="https://graph.org/file/a642be9214ef87d401de6-319bfe9ae1703e55bf.jpg",
+        thumb_url="https://files.catbox.moe/gi7f5r.jpg",
         input_message_content=InputTextMessageContent("/pause"),
     ),
     InlineQueryResultArticle(
         title="• ʀᴇsᴜᴍᴇ",
         description="ʀᴇsᴜᴍᴇ ᴛʜᴇ ᴩᴀᴜsᴇᴅ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-        thumb_url="https://graph.org/file/a642be9214ef87d401de6-319bfe9ae1703e55bf.jpg",
+        thumb_url="https://files.catbox.moe/gi7f5r.jpg",
         input_message_content=InputTextMessageContent("/resume"),
     ),
     InlineQueryResultArticle(
         title="• sᴋɪᴩ",
         description="sᴋɪᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀɴᴅ ᴍᴏᴠᴇs ᴛᴏ ᴛʜᴇ ɴᴇxᴛ sᴛʀᴇᴀᴍ.",
-        thumb_url="https://graph.org/file/a642be9214ef87d401de6-319bfe9ae1703e55bf.jpg",
+        thumb_url="https://files.catbox.moe/gi7f5r.jpg",
         input_message_content=InputTextMessageContent("/skip"),
     ),
     InlineQueryResultArticle(
         title="🥺 ᴇɴᴅ",
         description="ᴇɴᴅ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-        thumb_url="https://graph.org/file/a642be9214ef87d401de6-319bfe9ae1703e55bf.jpg",
+        thumb_url="https://files.catbox.moe/gi7f5r.jpg",
         input_message_content=InputTextMessageContent("/end"),
     ),
     InlineQueryResultArticle(
         title="• sʜᴜғғʟᴇ",
         description="sʜᴜғғʟᴇ ᴛʜᴇ ǫᴜᴇᴜᴇᴅ sᴏɴɢs ɪɴ ᴩʟᴀʏʟɪsᴛ.",
-        thumb_url="https://graph.org/file/a642be9214ef87d401de6-319bfe9ae1703e55bf.jpg",
+        thumb_url="https://files.catbox.moe/gi7f5r.jpg",
         input_message_content=InputTextMessageContent("/shuffle"),
     ),
     InlineQueryResultArticle(
         title="• ʟᴏᴏᴩ",
         description="ʟᴏᴏᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ ᴛʀᴀᴄᴋ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-        thumb_url="https://graph.org/file/a642be9214ef87d401de6-319bfe9ae1703e55bf.jpg",
+        thumb_url="https://files.catbox.moe/gi7f5r.jpg",
         input_message_content=InputTextMessageContent("/loop 3"),
     ),
 ]
